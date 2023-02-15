d232484fd3412bd4114a97f0cbda5e00790fa70f
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-02-15 22:30:03 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,125 @@
+//-------------------1-masala----------------
+// let myErray = [];
+// let myNew = [];
+// for (let i = 1; i < 6; i++) {
+//     myErray = +prompt();
+//     if (isNaN(myErray) || myErray == "") {
+//         alert("son kiriting")
+//     } else {
+//         myNew.push(myErray)
+//     }
+
+
+// }
+// const calcitp = function (sonlar) {
+//     let sums = 0;
+//     for (const iterator of sonlar) {
+//         sums = sums + iterator;
+//     }
+//     return sums;
+// }
+// console.log(calcitp(myNew));
+
+// console.log(myNew);
+
+
+
+
+
+//------------2-masal-------------------
+// const newArray = ['jazz', 'blues'];
+// console.log(newArray);
+
+// newArray.push('Rock-n-roll');
+// console.log(newArray);
+
+// newArray.splice(1, 1, 'classic')
+// console.log(newArray);
+// newArray.shift();
+// console.log(newArray);
+// newArray.unshift('Rap', 'Rejjai');
+// console.log(newArray);
+
+
+
+
+
+
+//--------------------3-masala--------------------
+
+
+// let salaries = {
+//     "Bob": 1700,
+//     "John": 6000,
+//     'evilene': 1200,
+//     griting: function () {
+//         console.log(`${salaries.Bob + salaries.John + salaries.evilene} `);
+//     }
+// }
+
+// salaries.griting();
+
+
+
+
+
+//---------------------4-masala-----------------
+// let namber = [1, 2, 3, 4, 5, 6, 7, 8, 9,11,222,44,555]
+
+// let arrr = [];
+// let arrr1 = [];
+// let arrr2 = [];
+// let arrr3 = [];
+// function myFun4(nums) {
+//     for (const iterator of nums) {
+//         if (iterator % 2 == 0) {
+//             arrr.push(iterator);
+//         } else {
+//             arrr1.push(iterator)
+//         }
+//     }
+//     console.log(arrr);
+//     console.log(arrr1);
+// }
+
+// myFun4(namber)
+// function arrr1Fun(arrr1) {
+//     let sams = 0;
+//     for (const iterator of arrr1) {
+//         sams = sams + iterator
+//     }
+//     return sams;
+// }
+// arrr2.push(arrr1Fun(arrr1))
+
+// function arrrFun(arrr) {
+//     let sams = 0;
+//     for (const iterator of arrr) {
+//         sams = sams + iterator
+//     }
+//     return sams;
+// }
+// arrr2.push(arrr1Fun(arrr))
+// console.log(arrr2);
+
+
+
+
+
+
+
+
+
+
+//--------------masla5-------------
+// const numbers = [-2, -1, 0, 5];
+
+// const result = [];
+// const son = Math.abs(numbers[0]) + numbers[numbers.length - 1];
+
+// for (let i = 0; i <= son; i++) {
+//     result.push(numbers[0] + i);
+// }
+
+// console.log(result);
+
